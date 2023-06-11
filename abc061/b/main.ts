23025cc2f5174939453d1b6347d868cabfbe0e4e
masa9973 <masamichi.ishikawa19990730@gmail.com>
2023-06-12 01:22:52 +0900

    update

--- abc061/b/main.ts
+++ abc061/b/main.ts
@@ -1,3 +1,18 @@
 import * as fs from "fs";
 const log = console.log;
-const i = fs.readFileSync("/dev/stdin", "utf8").split("\n");
+const i = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+
+const [N, M] = i[0].split(" ").map((i) => parseInt(i));
+const abl = i.slice(1).map((i) => i.split(" ").map((j) => parseInt(j)));
+
+const ans = Array(N).fill(0);
+for (let i = 0; i < M; i++) {
+    const a = abl[i][0];
+    const b = abl[i][1];
+    ans[a - 1]++;
+    ans[b - 1]++;
+}
+
+ans.forEach((i) => {
+    log(i);
+});
