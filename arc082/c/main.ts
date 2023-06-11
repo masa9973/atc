15de66dc69a7c4fc2b7e9ee7c3b3d97303f5292e
masa9973 <masamichi.ishikawa19990730@gmail.com>
2023-06-11 19:00:11 +0900

    arc082c

--- arc082/c/main.ts
+++ arc082/c/main.ts
@@ -1,3 +1,20 @@
 import * as fs from "fs";
 const log = console.log;
 const i = fs.readFileSync("/dev/stdin", "utf8").split("\n");
+
+const N = parseInt(i[0]);
+
+const a = i[1].split(" ").map((i) => parseInt(i));
+
+const xl = Array(100002).fill(0);
+for (let i = 0; i < N; i++) {
+    xl[a[i]]++;
+}
+
+let ans = 0;
+for (let x = 0; x < 100000; x++) {
+    const count = xl[x] + xl[x + 1] + xl[x + 2];
+    ans = Math.max(count, ans); // 個数を更新
+}
+
+log(ans);
